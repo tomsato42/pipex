51bfd0ef0ef997f3f5f8f8722c0816a28a46300f
kinakqna <tomokirenrakuyo@gmail.com>
2025-03-07 12:01:53 +0900

    ちょっと行数削った

--- playground/pipe/easy_pipe.c
+++ playground/pipe/easy_pipe.c
@@ -6,7 +6,7 @@
 /*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 19:58:22 by tomsato           #+#    #+#             */
-/*   Updated: 2025/03/06 19:58:23 by tomsato          ###   ########.fr       */
+/*   Updated: 2025/03/07 10:37:42 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,35 +30,30 @@ char	**get_execve_argv(const char *cmd, const char *option)
 	return (result);
 }
 
+void	exit_with_error(char *error)
+{
+	perror(error);
+	exit(EXIT_FAILURE);
+}
+
 int	main(int ac, char **av)
 {
-	int		pipefd[2];
-	pid_t	cpid_1;
-	pid_t	cpid_2;
-	char	**execve_argv;
-	char	*cmd_1;
-	char	*cmd_1_option;
-	char	*cmd_2;
-	char	*cmd_2_option;
+	int			pipefd[2];
+	pid_t		cpid_1;
+	pid_t		cpid_2;
+	char		**execve_argv;
+	char		*cmd_1;
+	char		*cmd_1_option;
+	char		*cmd_2;
+	char		*cmd_2_option;
+	char *const	envp[] = {NULL};
 
-	// const char	*cmd_1 = "/bin/ls";
-	// const char	*cmd_1_option = "-l";
-	// const char	*cmd_2 = "/bin/wc";
-	// const char	*cmd_2_option = "-l";
-	if (ac != 5) // pipexは通常5つの引数が必要
-	{
-		//エラーメッセージは今のところめちゃくちゃ
-		fprintf(stderr, "Error: insufficient arguments\n");
-		fprintf(stderr, "Usage: %s file1 cmd1 cmd2 file2\n", av[0]);
-		fprintf(stderr, "Example: %s infile \"ls -l\" \"wc -l\" outfile\n",
-			av[0]);
-		exit(EXIT_FAILURE);
-	}
+	if (ac != 5)
+		exit_with_error("insufficient arguments");
 	cmd_1 = av[1];
 	cmd_1_option = av[2];
 	cmd_2 = av[3];
 	cmd_2_option = av[4];
-	char *const envp[] = {NULL};
 	if (pipe(pipefd) == -1)
 	{
 		perror("pipe");
@@ -69,23 +64,14 @@ int	main(int ac, char **av)
 	{
 		close(pipefd[0]);
 		if (dup2(pipefd[1], STDOUT_FILENO) < 0)
-		{
-			perror("cmd1 dup2");
-			exit(EXIT_FAILURE);
-		}
+			exit_with_error("cmd1 dup2");
 		close(pipefd[0]);
 		execve_argv = get_execve_argv(cmd_1, cmd_1_option);
 		if (!execve_argv)
-		{
-			perror("malloc");
-			exit(EXIT_FAILURE);
-		}
+			exit_with_error("malloc");
 		if (execve(cmd_1, (char *const *)get_execve_argv(cmd_1, cmd_1_option),
 				envp) == -1)
-		{
-			perror("execve cmd1");
-			exit(EXIT_FAILURE);
-		}
+			exit_with_error("execve cmd1");
 		perror("execve cmd1");
 		free(execve_argv[0]);
 		free(execve_argv[1]);
@@ -97,19 +83,12 @@ int	main(int ac, char **av)
 	{
 		close(pipefd[1]);
 		if (dup2(pipefd[0], STDIN_FILENO) < 0)
-		{
-			perror("cmd2 dup2");
-			close(pipefd[0]);
-			exit(EXIT_FAILURE);
-		}
+			exit_with_error("cmd2 dup2");
 		close(pipefd[1]);
 		execve_argv = get_execve_argv(cmd_1, cmd_1_option);
 		if (execve(cmd_2, (char *const *)get_execve_argv(cmd_2, cmd_2_option),
 				envp) == -1)
-		{
-			perror("execve cmd2");
-			exit(EXIT_FAILURE);
-		}
+			exit_with_error("execve cmd1");
 		perror("execve cmd2");
 		free(execve_argv[0]);
 		free(execve_argv[1]);
