500374e752b859635bb12e126571a14be1f6cdb1
kinakqna <tomokirenrakuyo@gmail.com>
2025-03-21 13:51:42 +0900

    refactor: update perse_input.c and add pipex.h for execve argument handling

--- src/perse_input.c
+++ src/perse_input.c
@@ -1,39 +1,25 @@
-#include "../libft/libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   perse_input.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 13:19:57 by tomsato           #+#    #+#             */
+/*   Updated: 2025/03/21 13:48:38 by tomsato          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
 
 /*
-input -> argc,argv
-
-1-> ac - 1
-2-> commands
-3-> null or options
-output -> char ***
+Take an array and an index as arguments.
+Convert the selected element into the format required by execve.
+This can be easily achieved using split.
 */
 
-typedef struct s_command
+char	**get_execve_argv(char **av, size_t index)
 {
-	char	*command;
-	char	*option;
-} t_command;
-
-t_command	*parce_input(int ac, char **av)
-{
-	size_t	i;
-	char	*space_point;
-	t_command	*return_value;
-
-	return_value = (t_command *)malloc(sizeof(t_command) * (ac - 1));
-	i = 1;
-	while(i < ac)
-	{
-		while(ft_isspace(av[i]))
-			av[i]++;
-		space_point = ft_strchr(av[i], ' ');
-		return_value->command = ft_substr(av[i], 0, space_point - av[i]);
-		av[i] += space_point - av[i];
-		while(ft_isspace(av[i]))
-			av[i]++;
-		
-	}
-
-	return ;
+	const char** result = ft_split(av[index], ' ');
+	return result;
 }
